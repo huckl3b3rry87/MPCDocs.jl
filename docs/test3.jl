e79484e791f56732c5b3a8b884810f9e19fc90c3
Huckleberry Febbo <febbo@umich.edu>
2017-06-09 18:08:16 -0400

    modified .travis file to avoid segfault

--- docs/test3.jl
+++ docs/test3.jl
@@ -2,6 +2,8 @@ using NLOptControl,JuMP,PrettyPlots,Plots;gr()
 
 
 n=define!(;numStates=3,numControls=1,X0=[0.0,0.0,0.0],XF=[2.,2.,NaN],XL=[-NaN,-NaN,-NaN],XU=[NaN,NaN,NaN],CL=[-NaN],CU=[NaN]);
+#eq=:([x[j,3]*sin(u[j,1]);x[j,3]*cos(u[j,1]);9.81*cos(u[j,1])]);
+#@show typeof(n)
+#@show typeof(eq)
 @DiffEq(n,[x[j,3]*sin(u[j,1]);x[j,3]*cos(u[j,1]);9.81*cos(u[j,1])])
-
 configure!(n,Nck=[100];(:finalTimeDV=>true));
