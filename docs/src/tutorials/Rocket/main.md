b2b03ace6c7cf93a56b5010259628bf93860beeb
Huckleberry Febbo <febbo@umich.edu>
2017-06-10 20:35:25 -0400

    modified .travis file again to try and fix pgfplots()

---
+++ docs/src/tutorials/Rocket/main.md
@@ -0,0 +1,75 @@
+# Rocket
+
+This problem can be found [here](https://github.com/JuliaOpt/juliaopt-notebooks/blob/master/notebooks/JuMP-Rocket.ipynb).
+
+
+## Packages that will be used
+```@example Rocket
+using NLOptControl,JuMP,PrettyPlots,Plots;pgfplots()
+nothing # hide
+```
+
+## Differential Equations
+```@example Rocket
+# Constants
+# Note that all parameters in the model have been normalized
+# to be dimensionless. See the COPS3 paper for more info.
+h_0 = 1    # Initial height
+v_0 = 0    # Initial velocity
+m_0 = 1    # Initial mass
+g_0 = 1    # Gravity at the surface
+
+# Parameters
+T_c = 3.5  # Used for thrust
+h_c = 500  # Used for drag
+v_c = 620  # Used for drag
+m_c = 0.6  # Fraction of initial mass left at end
+
+# Derived parameters
+c     = 0.5*sqrt(g_0*h_0)  # Thrust-to-fuel mass
+m_f   = m_c*m_0            # Final mass
+D_c   = 0.5*v_c*m_0/g_0    # Drag scaling
+T_max = T_c*g_0*m_0        # Maximum thrust
+
+const EP=2*eps()
+function Rocket{T<:Any}(n::NLOpt,x::Array{T,2},u::Array{T,2}) # dynamic constraint equations
+  if n.s.integrationMethod==:tm; L=size(x)[1]; else; L=size(x)[1]-1; end
+  dx=Array(Any,L,n.numStates);
+  Q=5;
+  I_t=@NLexpression(n.mdl, [j=1:L], ((Q-x[j,1])^3+x[j,1]^3)/3*sin(x[j,5])^2 );
+  I_p=@NLexpression(n.mdl, [j=1:L], ((Q-x[j,1])^3+x[j,1]^3)/3 );
+
+  dx[:,1]=@NLexpression(n.mdl, [j=1:L], x[j,2]);
+  dx[:,2]=@NLexpression(n.mdl, [j=1:L], u[j,1]/Q);
+  dx[:,3]=@NLexpression(n.mdl, [j=1:L], x[j,4]);
+  dx[:,4]=@NLexpression(n.mdl, [j=1:L], u[j,2]/(I_t[j]+EP));
+  dx[:,5]=@NLexpression(n.mdl, [j=1:L], x[j,6]);
+  dx[:,6]=@NLexpression(n.mdl, [j=1:L], u[j,3]/(I_p[j]+EP));
+  return dx
+end
+nothing # hide
+```
+
+## Define and Configure the Problem:
+```@example Rocket
+n=define!(;stateEquations=Rocket,numStates=6,numControls=3,X0=[9/2,0.0,0.0,0.0,pi/4,0.0],XF=[9/2,0.0,2*pi/3,0.0,pi/4,0.0],XL=[NaN,NaN,NaN,0.0,NaN,NaN],XU=[NaN,NaN,NaN,1.0,NaN,NaN],CL=[-1.,-1.,-1.],CU=[1.,1.,1.])
+configure!(n;(:finalTimeDV=>true))
+nothing # hide
+```
+
+## Objective Function
+```@example Rocket
+@NLobjective(n.mdl,Min,n.tf);
+nothing # hide
+```
+
+## Optimize
+```@example Rocket
+optimize!(n);
+nothing # hide
+```
+
+## Post Process
+```@example Rocket
+allPlots(n)
+```
--- docs/test2.jl
+++ docs/test2.jl
@@ -1,33 +1,44 @@
 using NLOptControl,JuMP,PrettyPlots,Plots;gr()
 
-#@DiffEq(Brachistochrone,[x[j,3]*sin(u[j,1]);x[j,3]*cos(u[j,1]);9.81*cos(u[j,1])])
-#@DiffEq(Brachistochrone,[u[j,1];u[j,1];u[j,1]])
-#$(refcall) =
+# Constants
+# Note that all parameters in the model have been normalized
+# to be dimensionless. See the COPS3 paper for more info.
+h_0 = 1    # Initial height
+v_0 = 0    # Initial velocity
+m_0 = 1    # Initial mass
+g_0 = 1    # Gravity at the surface
 
-#JuMP.NonlinearExpression($m, @JuMP.processNLExpr($m, $(esc(x))))
-#=
-ex = JuMP.@processNLExpr($m, $(esc(x)))
-dx=Array(JuMP.NonlinearExpression,3,1);
-dx[1]=:($(n.r.x)[j,3]*sin($(n.r.u)[j,1]));
-dx[2]=:($(n.r.x)[j,3]*cos($(n.r.u)[j,1]));
-dx[3]=:(9.81*cos($(n.r.u)[j,1]));
-=#
-#@DiffEq(Brachistochrone,[$(x[j,3])*sin(u[j,1]);$(x[j,3])*cos($(u[j,1]));9.81*cos($(u[j,1]))])  # can do this afterwards
+# Parameters
+T_c = 3.5  # Used for thrust
+h_c = 500  # Used for drag
+v_c = 620  # Used for drag
+m_c = 0.6  # Fraction of initial mass left at end
 
-function Brachistochrone{T<:Any}(n::NLOpt,x::Array{T,2},u::Array{T,2})
+# Derived parameters
+c     = 0.5*sqrt(g_0*h_0)  # Thrust-to-fuel mass
+m_f   = m_c*m_0            # Final mass
+D_c   = 0.5*v_c*m_0/g_0    # Drag scaling
+T_max = T_c*g_0*m_0        # Maximum thrust
+
+# Forces
+# Drag(h,v) = Dc v^2 exp( -hc * (h - h0) / h0 )
+@NLexpression(mod, drag[j=0:n], D_c*(v[j]^2)*exp(-h_c*(h[j]-h_0)/h_0))
+# Grav(h)   = go * (h0 / h)^2
+@NLexpression(mod, grav[j=0:n], g_0*(h_0/h[j])^2)
+# Time of flight
+@NLexpression(mod, t_f, Δt*n)
+
+function Rocket{T<:Any}(n::NLOpt,x::Array{T,2},u::Array{T,2})
   if n.s.integrationMethod==:tm; L=size(x)[1]; else L=size(x)[1]-1; end
   dx = Array(Any,L,n.numStates)
-  dx[:,1]=@NLexpression(n.mdl, [j=1:L], x[j,3]*sin(u[j,1]) );
-  dx[:,2]=@NLexpression(n.mdl, [j=1:L], x[j,3]*cos(u[j,1]) );
-  dx[:,3]=@NLexpression(n.mdl, [j=1:L], 9.81*cos(u[j,1]) );
+  dx[:,1]=@NLexpression(n.mdl, [j=1:L], x[j,2] );
+  dx[:,2]=@NLexpression(n.mdl, [j=1:L], (T[j]-drag[j])/m[j]-grav[j] );
+  dx[:,3]=@NLexpression(n.mdl, [j=1:L], -T[j]/c );
   return dx
 end
 
-n=define!(stateEquations=Brachistochrone;numStates=3,numControls=1,X0=[0.0,0.0,0.0],XF=[2.,2.,NaN],XL=[-NaN,-NaN,-NaN],XU=[NaN,NaN,NaN],CL=[-NaN],CU=[NaN]);
+n=define!(stateEquations=Rocket;numStates=3,numControls=1,X0=[0.0,0.0,0.0],XF=[2.,2.,NaN],XL=[-NaN,-NaN,-NaN],XU=[NaN,NaN,NaN],CL=[-NaN],CU=[NaN]);
 configure!(n,Nck=[100];(:finalTimeDV=>true));
-
-#configure!(n,N=30;(:integrationMethod=>:tm),(:integrationScheme=>:trapezoidal),(:finalTimeDV=>true));
-#configure!(n,Ni=4,Nck=[10,10,10,10];(:integrationMethod=>:ps),(:integrationScheme=>:lgrExplicit),(:finalTimeDV=>true),(:tf=>4));
 names=[:x,:y,:v]; descriptions=["x(t)","y(t)","v(t)"];
 stateNames!(n,names,descriptions);
 names=[:u]; descriptions=["u(t)"];
